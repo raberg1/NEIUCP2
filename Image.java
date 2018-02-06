2255e31f0ec1331ff7f824ebcd33c4dde6e7b5d3
Robert Berg <raberg1@gmail.com>
2018-02-05 20:27:01 -0600

    Completed the read method

--- Image.java
+++ Image.java
@@ -7,64 +7,70 @@ import javax.imageio.stream.*;
 
 public class Image 
 {
-	private int width, height; 
-	private int[][] pixels;
-   
-	public Image(String filename) throws Exception 
-	{ 
-		this.read(filename); 
-	}
-	
-	public int getWidth()
-	{
-		return this.width;
-	}
-	
-	public int getHeight()
-	{
-		return this.height;
-	}
-   
-	public void read(String filename) throws Exception 
-	{
-		File fileImage = new File(filename);
-        
+    private int width, height; 
+    private int[][] pixels;
+
+    public Image(String filename) throws Exception 
+    { 
+        this.read(filename); 
+    }
+
+    public int getWidth()
+    {
+        return this.width;
+    }
+
+    public int getHeight()
+    {
+        return this.height;
+    }
+
+    public void read(String filename) throws Exception 
+    {
+        File fileImage = new File(filename);
+
         BufferedImage bufImage = ImageIO.read(fileImage);
-		this.width = bufImage.getWidth();
-		this.height = bufImage.getHeight();
-		
-		// Complete the remainder of this method 
-         
-   }
-   
-   private BufferedImage createBufferedImage() 
-   	{ 
-      	BufferedImage bufImage = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
-  
-      	for (int row = 0; row < this.height; row++)
-      	{
-         	for (int col = 0; col < this.width; col++)
-         	{
-         		bufImage.setRGB(col, row, this.pixels[row][col]);
-         	}
+        this.width = bufImage.getWidth();
+        this.height = bufImage.getHeight();
+
+        // Complete the remainder of this method
+        this.pixels = new int[this.height][this.width];
+        
+        for(int row = 0; row < this.height; row++){
+            for(int col = 0; col < this.width; col++){
+                this.pixels[row][col] = bufImage.getRGB(col, row);
+            }
         }
-         
-      	return bufImage;
-	}
-   
-   public void write(String filename) throws Exception 
-   {
+
+    }
+
+    private BufferedImage createBufferedImage() 
+    { 
+        BufferedImage bufImage = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
+
+        for (int row = 0; row < this.height; row++)
+        {
+            for (int col = 0; col < this.width; col++)
+            {
+                bufImage.setRGB(col, row, this.pixels[row][col]);
+            }
+        }
+
+        return bufImage;
+    }
+
+    public void write(String filename) throws Exception 
+    {
         File fileImage = new File(filename);
         String ext = filename.substring(filename.indexOf('.') + 1);
         BufferedImage bufImage = createBufferedImage();
         ImageIO.write(bufImage, ext, fileImage);
-   }
-   
-   
-	public void draw(Graphics gc, int x, int y)
-	{
-   		BufferedImage bufImage = createBufferedImage();
-      	gc.drawImage(bufImage, x, y, null);
-   	}
+    }
+
+    public void draw(Graphics gc, int x, int y)
+    {
+        BufferedImage bufImage = createBufferedImage();
+        gc.drawImage(bufImage, x, y, null);
+    }
 
 } 
