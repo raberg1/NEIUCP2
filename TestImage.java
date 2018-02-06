34c9d007c0137bf96848204867cc69265bada416
Robert Berg <raberg1@gmail.com>
2018-02-05 20:31:54 -0600

    Test original method with animals.jpg

--- TestImage.java
+++ TestImage.java
@@ -3,20 +3,21 @@ import javax.imageio.*;
 
 class TestImage 
 {
-	public static void main(String args[]) throws Exception 
-	{
-    	Frame frame = new Frame("My Images");
-      	frame.setSize(1024,768);
-      	frame.setVisible(true);
-      	Graphics gc = frame.getGraphics();
-      
-      	try 
-      	{
-         	
-		} 
-		catch (Exception e)
-		{
-         	e.printStackTrace();
-      	}
-	}
+    public static void main(String args[]) throws Exception 
+    {
+        Frame frame = new Frame("My Images");
+        frame.setSize(1024,768);
+        frame.setVisible(true);
+        Graphics gc = frame.getGraphics();
+
+        try 
+        {
+            Image img = new Image("animals.jpg");
+            img.draw(gc, 10, 40);
+        } 
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
